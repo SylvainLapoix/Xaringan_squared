bb2cf637f9d0a8c9c42d06b07ec590baa3553280
Sylvain Lapoix <sylvainlapoix@gmail.com>
2019-08-03 14:30:24 +0200

    Revert "petites modifs aux slides"

--- macros.js
+++
@@ -1,8 +0,0 @@
-remark.macros.largeur = function (percentage) {
-  var url = this;
-  return '<img src="' + url + '" style="width: ' + percentage + '" />';
-};
-remark.macros.hauteur = function (pixels) {
-  var url = this;
-  return '<img src="' + url + '" style="height: ' + pixels + '" />';
-};
